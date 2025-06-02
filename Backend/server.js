b329d40efecbbb49866646d477989c105e69c8d1
sadoozii <sadoozii@sadoozii>
2025-06-02 22:34:03 +0500

    worked on deployment , maintained notes , as we arenot going to deploy this project completely

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@
 //& we had  added type : "module" in package.json , so we can use import instead of require.
 import express from "express";
 import dotenv from "dotenv";
+
+
 import { connectDB } from "./Config/db.js";
 import cors from 'cors';
 
@@ -12,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 // import productModel from "./models/product.model.js";
 // import mongoose from "mongoose";
 import productRoutes from "./Routes/product_routes.js";
-
 
 const app = express();
 app.use(cors());
@@ -29,6 +30,9 @@ app.use(express.json());
 
 //& /api/products is a base URL .it will be present in the URL even if we don't add it in the postman/router.
 app.use("/api/products", productRoutes);
+
+
+
 
 
 app.listen(PORT, () => {
